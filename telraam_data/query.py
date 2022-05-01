ddff1564b18f65d0aab5fffdf851c9e9c1716e76
Paul Daum <paul.daum@posteo.de>
2022-05-01 11:28:35 +0200

    - query api now uses datetime dates instead of strings - queries with time-intervals of over 90 days are now split up into multiple queries and merged together locally.

--- telraam_data/query.py
+++ telraam_data/query.py
@@ -1,7 +1,9 @@
 """Functions to retrieve data from the Telraam API."""
 import requests
+import datetime as dt
 from typing import Dict, Optional
 from . import log
+from tqdm.auto import tqdm
 import os
 
 TELRAAM_API_URL = "https://telraam-api.net/v1"
@@ -83,9 +85,9 @@ def query_active_segments_in_radius(
 
 def query_one_segment(
         segment_id: str,
-        time_start: str,
-        time_end: str,
-        api_token: Optional[str] = ENVVAR_TELRAAM_API_TOKEN
+        start_date: dt.date,
+        end_date: dt.date,
+        api_token: Optional[str] = None
 ) -> Dict:
     """Returns traffic information for one segment.
 
@@ -93,12 +95,13 @@ def query_one_segment(
     ----------
     segment_id : str
         Unique segment identifier (e.g. "1003073114").
-    time_start : str
-        Start time in "YYYY-MM-DD HH:MM:SSZ" format (e.g "2020-01-01 00:00:00Z").
-    time_end : str
-        End time in the same format as `time_start`.
+    start_date : datetime.date
+        Start date of the desired data.
+    end_date : datetime.date
+        End date of the desired data.
     api_token: str
         Your personal Telraam API token.
+        Defaults to the environment variable TELRAAM_API_TOKEN.
 
     Returns
     -------
@@ -107,14 +110,40 @@ def query_one_segment(
     """
     url = f"{TELRAAM_API_URL}/reports/traffic"
     headers = {'X-Api-Key': ENVVAR_TELRAAM_API_TOKEN if api_token is None else api_token}
-    payload = str({
-        "time_start": time_start,
-        "time_end": time_end,
-        "level": "segments",
-        "format": "per-hour",
-        "id": segment_id
-    })
-    log.debug(f"Querying {url} with data: {payload}")
-    response = requests.post(url, headers=headers, data=payload)
-    check_response_health(response)
-    return response.json()
+
+    # Query can be 92 days long max
+    # Get time-intervals that shall be downloaded one by one
+    time_step = dt.timedelta(days=90)
+    dates = [start_date]
+    while dates[-1] < end_date:
+        dates.append(dates[-1] + time_step)
+
+    # Query all data intervals
+    responses = []
+    for i in tqdm(range(len(dates) - 1), desc=f"Downloading data from {start_date} to {end_date}"):
+        start_date = dates[i]
+        end_date = dates[i + 1]
+        payload = str({
+            "time_start": start_date.strftime("%Y-%m-%d 00:00:00Z"),
+            "time_end": end_date.strftime("%Y-%m-%d 23:59:59Z"),
+            "level": "segments",
+            "format": "per-hour",
+            "id": segment_id
+        })
+        log.debug(f"Querying {url} with data: {payload}")
+        response = requests.post(url, headers=headers, data=payload)
+        if _response_is_healthy(response):
+            responses.append(response)
+        else:
+            log.info(f"No data found for time interval {start_date} to {end_date}")
+
+    if len(responses) == 0:
+        log.info(f"Data could not be queried.")
+        return None
+
+    # Assemble data intervals
+    json = {'status_code': responses[0].json()['status_code'], 'message': responses[0].json()['message'], 'report': []}
+    for response in responses:
+        json['report'].extend(response.json()['report'])
+
+    return json
