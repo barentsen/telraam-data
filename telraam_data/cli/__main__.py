19d42f06f1ffb3f17d24556b6328b8a309e0db38
Paul Daum <paul.daum@posteo.de>
2022-06-12 12:55:30 +0200

    Made command-line interface usable, added more input checks and added paramter --output-path,

--- telraam_data/cli/__main__.py
+++ telraam_data/cli/__main__.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 from telraam_data.query import _response_is_healthy, TELRAAM_API_URL, ENVVAR_TELRAAM_API_TOKEN
 import click
 import requests
+import telraam_data
 
 
 @click.group()
@@ -11,51 +12,76 @@ def cli():
 
 @click.command()
 @click.argument('segment-id', type=str)
-@click.option('--start-date',
+@click.option('-s', '--start-date',
               type=click.DateTime(formats=["%Y-%m-%d"]),
               default=None,  # defaults to earliest data point during runtime
+              help="The beginning of the desired time interval. Defaults to the first day of the"
+                   "specified Telraam sensor's activity."
               )
-@click.option('--end-date',
+@click.option('-e', '--end-date',
               type=click.DateTime(formats=["%Y-%m-%d"]),
-              default=str(date.today())
+              default=str(date.today()),
+              help="The end of the desired time interval. Defaults to today."
               )
-@click.option('--api-key',
+@click.option('-k', '--api-key',
               type=str,
-              default=ENVVAR_TELRAAM_API_TOKEN)
-def download(segment_id, start_date, end_date, api_key):
-    """ TODO : Command Documentation
+              default=ENVVAR_TELRAAM_API_TOKEN,
+              help="Your personal Telraam API token. If not set, the content of the environment variable"
+                   " 'TELRAAM_API_TOKEN' is used."
+              )
+@click.option('-o', '--output-path',
+              type=click.Path(),
+              default=None,  # Path defaults to generated string during runtime
+              help="The path to the file where the data shall be stored."
+              )
+def download(segment_id, start_date, end_date, api_key, output_path):
+    """Downloads and stores Telraam data from a specified road segment.
     """
     if api_key is None:
-        click.echo("An API key is required.")
-        click.echo("Aborting.")
+        click.echo("An API key is required.\nAborting.")
         return
 
+    if start_date is not None:
+        if start_date > end_date:
+            click.echo("The start date must be earlier than the end date.\nAborting.")
+            return
+
     url = TELRAAM_API_URL + "/cameras/segment/" + segment_id
-    headers = {'X-Api-Key': ENVVAR_TELRAAM_API_TOKEN}
+    headers = {'X-Api-Key': api_key}
     response = requests.request("GET", url, headers=headers, data={})
 
     if not _response_is_healthy(response):
-        click.echo("Couldn't reach url " + url + ".")
-        click.echo("Aborting.")
+        if response.status_code == 403:
+            click.echo("API key was rejected by the Telraam server.\nAborting")
+        else:
+            click.echo("Couldn't reach url " + url + ".\nAborting.")
         return
 
     if len(response.json()["camera"]) == 0:
-        click.echo("No segment with id " + segment_id + " found.")
-        click.echo("Aborting.")
+        click.echo("No segment with id " + segment_id + " found.\nAborting.")
         return
 
     if start_date is None:
-        start_date = end_date
-        dates = [datetime.strptime(cam["first_data_package"], "%Y-%m-%dT%H:%M:%S.%fZ") for cam in response.json()["camera"]]
+        dates = [datetime.strptime(cam["first_data_package"], "%Y-%m-%dT%H:%M:%S.%fZ") for cam
+                 in response.json()["camera"]]
         start_date = min(dates)
 
-        #for camera in response.json()["camera"]:
-        #    first_data_package_date = datetime.strptime(camera["first_data_package"], "format")
-        #    if first_data_package_date < start_date:
-        #        start_date = first_data_package_date
+    if output_path is None:
+        output_path = "./telraam_data_" + segment_id
+        output_path += "_from_" + start_date.strftime("%Y-%m-%d")
+        output_path += "_to_" + end_date.strftime("%Y-%m-%d")
+        output_path += ".csv"
 
-    click.echo(f"Start: {start_date}, End: {end_date} ")
-    click.echo(f"Segment-id: {segment_id}, api-key: {api_key} ")
+    click.echo(f"Segment-id: {segment_id}")
+    click.echo(f"Start: {start_date.date()}, End: {end_date.date()} ")
+    click.echo(f"Output path: {output_path}")
+
+    telraam_data.download_one_segment(
+        segment_id=segment_id,
+        start_date=start_date,
+        end_date=end_date,
+        out_filepath=output_path,
+        api_token=api_key)
 
 
 cli.add_command(download)
