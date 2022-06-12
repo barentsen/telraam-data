6bcdd1fb496d8560aee63fcd9366f26a919531f9
Paul Daum <paul.daum@posteo.de>
2022-06-12 19:20:39 +0200

    Finished unit-tests for command-line interface.

--- telraam_data/cli/__main__.py
+++ telraam_data/cli/__main__.py
@@ -4,13 +4,6 @@ import click
 import requests
 import telraam_data
 import sys
-from enum import Enum
-
-
-class ExitCode(Enum):
-    SUCCESS = 0
-    SRV_ERROR = 1  # Server not reachable / client rejected / invalid server response
-    CLI_ERROR = 2  # Invalid user input to command-line interface
 
 
 @click.group()
@@ -47,13 +40,11 @@ def download(ctx, segment_id, start_date, end_date, api_key, output_path):
     """Downloads and stores Telraam data from a specified road segment.
     """
     if api_key is None:
-        click.echo("An API key is required.\nAborting.")
-        return ExitCode.CLI_ERROR
+        sys.exit("An API key is required.\nAborting.")
 
     if start_date is not None:
         if start_date > end_date:
-            click.echo("The start date must be earlier than the end date.\nAborting.")
-            return ExitCode.CLI_ERROR
+            sys.exit("The start date must be earlier than the end date.\nAborting.")
 
     url = TELRAAM_API_URL + "/cameras/segment/" + segment_id
     headers = {'X-Api-Key': api_key}
@@ -61,14 +52,11 @@ def download(ctx, segment_id, start_date, end_date, api_key, output_path):
 
     if not _response_is_healthy(response):
         if response.status_code == 403:
-            click.echo("API key was rejected by the Telraam server.\nAborting.")
-        else:
-            click.echo("Couldn't reach url " + url + ".\nAborting.")
-        sys.exit(ExitCode.SRV_ERROR)
+            sys.exit("API key was rejected by the Telraam server.\nAborting.")
+        sys.exit("Couldn't reach url " + url + ".\nAborting.")
 
     if len(response.json()["camera"]) == 0:
-        click.echo("No segment with id " + segment_id + " found.\nAborting.")
-        sys.exit(ExitCode.SRV_ERROR)
+        sys.exit("No segment with id " + segment_id + " found.\nAborting.")
 
     if start_date is None:
         dates = [datetime.strptime(cam["first_data_package"], "%Y-%m-%dT%H:%M:%S.%fZ") for cam
@@ -93,10 +81,7 @@ def download(ctx, segment_id, start_date, end_date, api_key, output_path):
         api_token=api_key)
 
     if df is None:
-        click.echo("An unexpected error happened.\nAborting.")
-        sys.exit(ExitCode.SRV_ERROR)
-
-    return ExitCode.SUCCESS
+        sys.exit("An unexpected error happened.\nAborting.")
 
 
 cli.add_command(download)
--- telraam_data/tests/test_cli.py
+++ telraam_data/tests/test_cli.py
@@ -1,41 +1,39 @@
 from click.testing import CliRunner
 from telraam_data.cli.__main__ import download as cli_download
-from telraam_data.query import ENVVAR_TELRAAM_API_TOKEN
+from telraam_data.query import ENVVAR_TELRAAM_API_TOKEN, TELRAAM_API_URL
 import telraam_data.query as query
+import datetime as dt
+import requests
 import pathlib
 import sys
-import shutil
 import random
 import pytest
 
 
 @pytest.fixture()
-def segment_id():
+def valid_segment_id():
     all_segments = query.query_active_segments()
     segment_idx = random.randrange(1, len(all_segments)) - 1
     return all_segments["features"][segment_idx]["properties"]["segment_id"]
 
 
 @pytest.fixture()
-def nonexisting_segment_id():
+def invalid_segment_id():
     existing_segments = query.query_active_segments()
     existing_segment_ids = [segment["properties"]["segment_id"] for segment in existing_segments["features"]]
-    nonexisting_id = random.randint(0, sys.maxsize)
-    while nonexisting_id in existing_segment_ids:
-        nonexisting_id = random.randint(0, sys.maxsize)
-    return nonexisting_id
+    some_id = random.randint(0, sys.maxsize)
+    while some_id in existing_segment_ids:
+        some_id = random.randint(0, sys.maxsize)
+    return some_id
 
 
 @pytest.fixture()
-def valid_date(segment_id):
-    from telraam_data.query import TELRAAM_API_URL
-    import requests
-    from datetime import datetime
-
-    url = TELRAAM_API_URL + "/cameras/segment/" + str(segment_id)
+def valid_date(valid_segment_id):
+    url = TELRAAM_API_URL + "/cameras/segment/" + str(valid_segment_id)
     headers = {'X-Api-Key': ENVVAR_TELRAAM_API_TOKEN}
     response = requests.request("GET", url, headers=headers, data={})
-    dates = [datetime.strptime(cam["first_data_package"], "%Y-%m-%dT%H:%M:%S.%fZ") for cam in response.json()["camera"]]
+    dates = [dt.datetime.strptime(cam["first_data_package"], "%Y-%m-%dT%H:%M:%S.%fZ") for cam
+             in response.json()["camera"]]
     return min(dates)
 
 
@@ -47,63 +45,62 @@ def output_path():
     path.unlink()
 
 
-def test_cli_success(segment_id, valid_date, output_path):
-    segment_id = str(segment_id)
-    valid_date = valid_date.strftime("%Y-%m-%d")
+def test_cli_success(valid_segment_id, valid_date, output_path):
+    valid_segment_id = str(valid_segment_id)
+    start_date = valid_date.strftime("%Y-%m-%d")
+    end_date = (valid_date + dt.timedelta(days=1)).strftime("%Y-%m-%d")
     runner = CliRunner()
     result = runner.invoke(cli_download, [
-        segment_id,
-        '--start-date', valid_date,
-        '--end-date', valid_date,
+        valid_segment_id,
+        '--start-date', start_date,
+        '--end-date', end_date,
         '--api-key', str(ENVVAR_TELRAAM_API_TOKEN),
         '--output-path', str(output_path)
     ])
     assert result.exit_code == 0
-    assert segment_id in result.output
-    assert valid_date in result.output
+    assert valid_segment_id in result.output
+    assert start_date in result.output
+    assert end_date in result.output
     assert str(output_path) in result.output
     assert "Downloading data" in result.output
 
 
-def test_cli_wrong_segment_id(nonexisting_segment_id, valid_date, output_path):
-    nonexisting_segment_id = str(nonexisting_segment_id)
+def test_cli_invalid_segment_id(invalid_segment_id, valid_date, output_path):
+    invalid_segment_id = str(invalid_segment_id)
     valid_date = valid_date.strftime("%Y-%m-%d")
     runner = CliRunner()
     result = runner.invoke(cli_download, [
-        nonexisting_segment_id,
+        invalid_segment_id,
         '--start-date', valid_date,
         '--end-date', valid_date,
         '--api-key', str(ENVVAR_TELRAAM_API_TOKEN),
         '--output-path', str(output_path)
     ])
     assert result.exit_code == 1
-    assert nonexisting_segment_id in result.output
+    assert invalid_segment_id in result.output
     assert "Aborting." in result.output
 
 
-def test_cli_wrong_dates(segment_id, valid_date, output_path):
-    segment_id = str(segment_id)
-    valid_date = valid_date.strftime("%Y-%m-%d")
+def test_cli_invalid_dates(valid_segment_id, valid_date, output_path):
+    valid_segment_id = str(valid_segment_id)
+    start_date = (valid_date + dt.timedelta(days=1)).strftime("%Y-%m-%d")
+    end_date = valid_date.strftime("%Y-%m-%d")
     runner = CliRunner()
     result = runner.invoke(cli_download, [
-        segment_id,
-        '--start-date', valid_date,
-        '--end-date', valid_date,
+        valid_segment_id,
+        '--start-date', start_date,
+        '--end-date', end_date,
         '--api-key', str(ENVVAR_TELRAAM_API_TOKEN),
         '--output-path', str(output_path)
     ])
-    assert result.exit_code == 0
-    assert segment_id in result.output
-    assert valid_date in result.output
-    assert str(output_path) in result.output
-    assert "Downloading data" in result.output
-    assert False
+    assert result.exit_code == 1
+    assert "start date must be earlier than the end date" in result.output
 
 
-def test_cli_wrong_api_keys(segment_id):
+def test_cli_invalid_api_key(valid_segment_id):
     runner = CliRunner()
     result = runner.invoke(cli_download, [
-        str(segment_id),
+        str(valid_segment_id),
         '--api-key', "This is probably an invalid API key :)",
     ])
     assert result.exit_code == 1
